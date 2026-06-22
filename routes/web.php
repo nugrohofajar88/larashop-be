24a2fea1253c800f9f2cccc72862b5236ae72401
Fajar Nugroho <fajar_nugroho@smltech.co.id>
2026-06-22 10:01:50 +0700

    feat: izinkan hapus QRIS aktif

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/__paths', function () {
+    return response()->json([
+        'document_root' => request()->server('DOCUMENT_ROOT'),
+        'public_path' => public_path(),
+        'base_path' => base_path(),
+        'app_url' => config('app.url'),
+        'uploads_root' => config('filesystems.disks.public_uploads.root'),
+    ]);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
